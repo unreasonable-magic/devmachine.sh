93c6233e2e1e54cd123250e429616d0c8dc546a5
Keith Pitt <me@keithpitt.com>
2025-05-21 00:59:52 +0800

    Added basic kitty theme config

--- tools/kitty.sh
+++ tools/kitty.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env devtool
 
+KITTY_CONFIG_PATH="${KITTY_CONFIG_PATH:-$HOME/.config/kitty}"
+
 case "$1" in
 
   setup)
     os::install "kitty"
+    os::linkfile "$DEVMACHINE_PATH/tools/kitty/kitty.conf" "$KITTY_CONFIG_PATH/kitty.conf"
+    ;;
+
+  reload)
+    pkill -USR1 -f kitty
+    ;;
+
+  config)
+    "$EDITOR" "$KITTY_CONFIG_PATH/kitty.conf"
     ;;
 
   --check-installed)
