fb633e1951f692b9fe7f6056c585351fcfbf943c
Keith Pitt <me@keithpitt.com>
2025-05-20 22:32:42 +0800

    Don't assume DEVMACHINE_PATH is present

--- cli/devmachine/tools.sh
+++ cli/devmachine/tools.sh
@@ -7,12 +7,12 @@ for t in "${DEVMACHINE_PATH}"/tools/*.sh; do
   # Now remove the extension
   t="${t/.sh/}"
 
-  check=$(devtool "$t" --check-installed)
+  check=$($DEVMACHINE_PATH/bin/devtool "$t" --check-installed)
 
   if [[ "$check" == "yes" ]]; then
     printf "\e[38;7m"
     printf "%s " "$t"
-    printf $(devtool "$t" --check-version)
+    printf $($DEVMACHINE_PATH/bin/devtool "$t" --check-version)
     printf "\e[0m\n"
   else
     printf "%s\n" "$t"
