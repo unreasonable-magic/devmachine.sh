4f76c8f5a690f4534ea6216bfe7314399c897c3b
Keith Pitt <me@keithpitt.com>
2025-05-21 00:41:58 +0800

    Added iterm2 support to the image renderer

--- lib/ui.sh
+++ lib/ui.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 ui::image() {
-  encoded=$(echo "$1" | tr -d '\n' | base64 | tr -d '=' | tr -d '\n')
+  path="$1"
   cols="$2"
   rows="$3"
 
-  printf "\n\e_Ga=T,q=2,f=100,t=f,c=$cols,r=$rows;%s\e\\ \n\n" "$encoded"
+  if [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
+    "$DEVMACHINE_PATH/lib/stdlib/image/iterm.sh" -W "${cols}" -H "${rows}" "$path"
+  else
+    encoded=$(echo "$path" | tr -d '\n' | base64 | tr -d '=' | tr -d '\n')
+
+    printf "\n\e_Ga=T,q=2,f=100,t=f,c=$cols,r=$rows;%s\e\\ \n\n" "$encoded"
+  fi
 }
