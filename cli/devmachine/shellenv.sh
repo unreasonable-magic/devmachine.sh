98d5bd68911f487bb8abdb73970a9539ebd728ec
Keith Pitt <git@keithpitt.email>
2025-05-20 17:23:09 +0800

    Moved more code into the cat call

--- cli/devmachine/shellenv.sh
+++ cli/devmachine/shellenv.sh
@@ -10,28 +10,23 @@ if [[ "$shell_name" == "" ]]; then
   exit
 fi
 
-# accessing $SHELL seems hit or miss sometimes, so
-# just always shove it in
-printf "export SHELL='%s'\n" $shell_name
-
-# make sure our own bin is part of PATH
-printf 'PATH="$PATH:%s"\n' "$DEVMACHINE_PATH/bin"
-
 os="$(uname -s)"
 if [[ "$os" == "Darwin" ]]; then
   cat "$DEVMACHINE_PATH/os/darwin.sh"
 fi
 
-echo '__devmachine_init_start="$(date +%s%N)"'
-
 cat <<- EOF
-cache_path="$XDG_CACHE_HOME/devmachine"
+export SHELL='$shell_name'
+
+PATH="\$PATH:$DEVMACHINE_PATH/bin"
+
+__devmachine_init_start="\$(date +%s%N)"
+
+cache_path="\$XDG_CACHE_HOME/devmachine"
 shellenv_cache_path="\$cache_path/shellenv_$shell_name.sh"
 
 if [[ -e "\$shellenv_cache_path" ]]; then
-
   source "\$shellenv_cache_path"
-
 else
   mkdir -p "\$cache_path"
 
@@ -43,9 +38,9 @@ else
     echo -E "\$shellenv" >> "\$shellenv_cache_path"
   done
 fi
+
+__devmachine_init_end="\$(date +%s%N)"
+export DEVMACHINE_INIT_RUNTIME="\$((__devmachine_init_end - __devmachine_init_start))"
+
+devtool $shell_name motd
 EOF
-
-echo '__devmachine_init_end="$(date +%s%N)"'
-echo 'export DEVMACHINE_INIT_RUNTIME="$((__devmachine_init_end - __devmachine_init_start))"'
-
-echo "devtool $shell_name motd"
