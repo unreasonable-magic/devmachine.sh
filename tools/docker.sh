96e04e439402bef331ec50d877f6b9e252b29321
Keith Pitt <me@keithpitt.com>
2025-05-21 00:30:30 +0800

    Fixed docker version grabbing

--- tools/docker.sh
+++ tools/docker.sh
@@ -11,7 +11,8 @@ case "$1" in
     ;;
 
   --check-version)
-    docker --version | head -1 | cut -d ' ' -f 3
+    # eg: Docker version 25.0.5, build 5dc9bcc
+    docker --version | head -1 | cut -d ' ' -f 3 | sed -e 's/[^0-9\.]//g'
     ;;
 
 esac
