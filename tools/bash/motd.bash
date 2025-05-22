ccdb25948a8722a589a85e87d549b2182018972e
Keith Pitt <git@keithpitt.email>
2025-05-22 15:05:11 +0800

    Move my devfiles to github.com/keithpitt/devfiles

--- tools/bash/motd.bash
+++
@@ -1,5 +0,0 @@
-# ms=$((DEVMACHINE_INIT_RUNTIME / 10000))
-
-# Print the thing
-# printf " Bash ${BASH_VERSION:0:3} \x1b[38;5;%sm%s\e[0m\n" "238" "(init ${ms}ms)"
-echo " Bash ${BASH_VERSION:0:3}"
--- tools/zsh/motd.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-# ms=$((DEVMACHINE_INIT_RUNTIME / 10000))
-
-# printf " Zsh ${ZSH_VERSION} \x1b[38;5;%sm%s\e[0m\n" "238" "(init ${ms}ms)"
-echo " Zsh ${ZSH_VERSION}"
