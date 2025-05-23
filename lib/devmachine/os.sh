e30669ec0b413805945138f6deb2aff9e054e88b
Keith Pitt <git@keithpitt.email>
2025-05-23 09:23:54 +0800

    Use a safer way of getting path for soft delete

--- lib/devmachine/os.sh
+++ lib/devmachine/os.sh
@@ -37,10 +37,34 @@ os::install() {
   esac
 }
 
+# This function takes a path and returns what it's full absolute version would
+# be. So ~/foo becomes /Users/name/foo, and ./blah becomes "$(PWD)/blah".  We
+# could use realpath mostly for this, but that errors out if the path doesn't
+# exist, which is kinda annoying to deal with, so this is just a slightly safer
+# way of doing it without the headache of errors.
+os::expandpath() {
+  local path="$1"
+  local first_char="${path:0:1}"
+
+  if [[ "$first_char" != "/" ]]; then
+    if [[ "$first_char" == "~" ]]; then
+      path="${HOME}${path:1}"
+    else
+      if [[ "$first_char" == "." ]]; then
+        path="$(pwd)/${path::1}"
+      else
+        path="$(pwd)/$path"
+      fi
+    fi
+  fi
+
+  echo "$path"
+}
+
 os::softdelete() {
   ui::logfunc "os::softdelete" "$@"
 
-  local path="$1"
+  local path="$(os::expandpath "$1")"
 
   if stdlib::test::exists "$path"; then
     new_path="$path.$(date +%Y%m%d%H%M%S).backup"
