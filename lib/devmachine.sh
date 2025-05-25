3761c80abc5ccd441336899a59022ee9d1ced8a2
Keith Pitt <me@keithpitt.com>
2025-05-25 23:35:59 +0800

    We can drop te .sh now our require gear handles it

--- lib/devmachine.sh
+++ lib/devmachine.sh
@@ -3,10 +3,11 @@
 eval "$(stdlib shellenv)"
 
 stdlib::require "stdlib.sh/error.sh"
-stdlib::require "stdlib.sh/test.sh"
-stdlib::require "stdlib.sh/image.sh"
-stdlib::require "stdlib.sh/array.sh"
-stdlib::require "stdlib.sh/color.sh"
+stdlib::require "stdlib.sh/test"
+stdlib::require "stdlib.sh/image"
+stdlib::require "stdlib.sh/array"
+stdlib::require "stdlib.sh/color"
+stdlib::require "stdlib.sh/debugger"
 
 source "$DEVMACHINE_PATH/lib/devmachine/version.sh"
 source "$DEVMACHINE_PATH/lib/devmachine/ui.sh"
