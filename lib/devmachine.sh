ea792459ed72a964fcbb1640a21ff2162ecca521
Keith Pitt <git@keithpitt.email>
2025-05-25 17:43:16 +0800

    Moved stdlib to another repo

--- lib/devmachine.sh
+++ lib/devmachine.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-source "$DEVMACHINE_PATH/lib/stdlib/error.sh"
-source "$DEVMACHINE_PATH/lib/stdlib/test.sh"
-source "$DEVMACHINE_PATH/lib/stdlib/image.sh"
-source "$DEVMACHINE_PATH/lib/stdlib/array.sh"
-source "$DEVMACHINE_PATH/lib/stdlib/color.sh"
+source "error.sh"
+source "test.sh"
+source "image.sh"
+source "array.sh"
+source "color.sh"
 
 source "$DEVMACHINE_PATH/lib/devmachine/version.sh"
 source "$DEVMACHINE_PATH/lib/devmachine/ui.sh"
--- lib/stdlib/array.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-stdlib::array::join() {
-  local IFS=$'\n'
-  shift
-  echo "$*"
-}
